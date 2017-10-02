b8cbb1570bf8474b1bd8c431ce6c246ea0c7cb38
hitodama <hitodama@users.noreply.github.com>
2017-10-02 21:41:49 +0200

    Fix non-rop system call targets includes

--- make/target/ps4_elf_sce.mk
+++ make/target/ps4_elf_sce.mk
@@ -9,7 +9,7 @@ include $(MakePath)/trait/link.mk
 
 include $(MakePath)/trait/sce.mk
 include $(MakePath)/trait/base.mk
-include $(MakePath)/trait/system_call_rop_standard.mk
+include $(MakePath)/trait/system_call_standard.mk
 
 ###################################
 
--- make/target/ps4_elf_standard.mk
+++ make/target/ps4_elf_standard.mk
@@ -13,7 +13,7 @@ include $(MakePath)/trait/adaptive.mk
 include $(MakePath)/trait/sce.mk
 include $(MakePath)/trait/kernel.mk
 include $(MakePath)/trait/base.mk
-include $(MakePath)/trait/system_call_rop_standard.mk
+include $(MakePath)/trait/system_call_standard.mk
 
 ###################################
 
