31338f0d490857a3f58e73f5ff7964261eb99750
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-01-04 17:26:48 +0000

    Add Pad Lightbar colour change support

--- include/sce/pad.h
+++ include/sce/pad.h
@@ -8,11 +8,12 @@ int scePadOpen(int userID, int, int, void *);
 int scePadClose(int handle);
 int scePadRead(int handle, void *data, int count);
 int scePadReadState(int handle, void *data);
+int scePadSetLightBar(int handle, uint32_t* rgb);
+int scePadResetLightBar(int handle);
 
 /* unreversed */
 int64_t scePadResetOrientation();
 int64_t scePadSetAngularVelocityDeadbandState();
-int64_t scePadSetLightBar();
 int64_t scePadSetMotionSensorState();
 int64_t scePadSetTiltCorrectionState();
 int64_t scePadSetVibration();
